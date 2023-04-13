da81102efc118d7f17b5b44c541368c6c33d3139
afsarahmad786 <mohommadafsar415@gmail.com>
2023-04-13 12:15:58 +0530

    env

--- chat/util/database.js
+++ chat/util/database.js
@@ -2,9 +2,14 @@ const Sequelize = require("sequelize");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const sequelize = new Sequelize("groupchat", "root", "root", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  "groupchat",
+  process.env.USER_NAME,
+  process.env.PASS_WORD,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST2,
+  }
+);
 
 module.exports = sequelize;
